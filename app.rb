bf9c1d85df3d23f267f04ca3dc74a813a5a6d5be
Alyssa Brown <alyssa.brown@live.com>
2019-04-24 02:25:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,19 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-   
-   erb :index
-  end 
-  
-  get '/new' do 
-    :create_puppy
-  end 
-  
-  post '/puppy' do 
+
+   get "/" do
+    erb :index
+  end
+
+   get "/new" do
+    erb :create_puppy
+  end
+
+   post "/puppy" do
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+
      erb :display_puppy
-     text_from_user = params[:user_text]
- 
-  end 
-end
+  end
+
+ end	
